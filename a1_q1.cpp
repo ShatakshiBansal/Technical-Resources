01e4e7f39f1c67a72b166b75a5afa7c706e051e6
ShatakshiBansal <113714550+ShatakshiBansal@users.noreply.github.com>
2023-01-19 19:34:51 +0530

    Submitting Q1 of Assignment 1

---
+++ a1_q1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+  //write your code here
+    cout<<"*****"<<endl;
+    cout<<"   *"<<endl;
+    cout<<"  *"<<endl;
+    cout<<" *"<<endl;
+    cout<<"*****";
+
+    return 0;
+
+}
